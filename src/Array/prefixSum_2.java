6efb08e18d040cd3132f00993a1d19d3a0e73e16
Ranjit Das <ranjitdasajodhyapur38@gmail.com>
2025-04-08 21:58:53 +0530

    Add prefix sum_2

---
+++ src/Array/prefixSum_2.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class prefixSum_2 {
+    static void prefixSumArray(int arr[]) {
+        int n = arr.length;
+
+        for(int i = 1; i < n; i++) {
+            arr[i] = arr[i] + arr[i-1];
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {2, 1, 3, 4, 5};
+
+        prefixSumArray(arr);
+        for(int val: arr) {
+            System.out.print(val+" ");
+        }
+    }
+}
